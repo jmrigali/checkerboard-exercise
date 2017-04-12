9250ec2e685c71bb24a661ee6f2434d25a72c8ec
jmrigali <jmrigali@ymail.com>
2017-04-12 16:49:37 -0600

    Random Colors

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ var body= document.getElementsByTagName('body')[0];
 
 for(var i = 0; i < 81; i++) {
 var tile= document.createElement('div');
-
+var color= randomColor();
 tile.style.width="11.1%";
 tile.style.float="left";
 tile.style.paddingBottom= "11.1%";
-if (i===40) {
-  tile.style.backgroundColor = "red";
-  body.appendChild(tile);
-} else if(i%2 === 0) {
-  tile.style.backgroundColor = "navy";
-  body.appendChild(tile);
-} else {
-  tile.style.backgroundColor = "white";
-  body.appendChild(tile);
+tile.style.backgroundColor= color;
+body.appendChild(tile);
 }
 
+function randomColor () {
+var letter="0123456789ABCDEF";
+var color="#";
+
+  for(var j = 0; j<6; j++) {
+    color += letter[Math.floor(Math.random() *16)];
+  }
+  return color;
 }
