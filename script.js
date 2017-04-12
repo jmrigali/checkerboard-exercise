9faeb9d58e0d876b759cf56dd7d72bd97886655d
jmrigali <jmrigali@ymail.com>
2017-04-12 16:12:12 -0600

    Another basic checkerboard

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ var tile= document.createElement('div');
 tile.style.width="11.1%";
 tile.style.float="left";
 tile.style.paddingBottom= "11.1%";
-
-if(i%2 === 0) {
+if (i===40) {
   tile.style.backgroundColor = "red";
   body.appendChild(tile);
+} else if(i%2 === 0) {
+  tile.style.backgroundColor = "navy";
+  body.appendChild(tile);
 } else {
-  tile.style.backgroundColor = "black";
+  tile.style.backgroundColor = "white";
   body.appendChild(tile);
 }
 
