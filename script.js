10148b2bccc651fc07f9c891a2bceca7b3951f0d
jmrigali <jmrigali@ymail.com>
2017-04-12 17:23:19 -0600

    Flashing checkerboard

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var body= document.getElementsByTagName('body')[0];
 
+function createCheckerboard () {
+  body.innerHTML="";
 for(var i = 0; i < 81; i++) {
-var tile= document.createElement('div');
-var color= randomColor();
-tile.style.width="11.1%";
-tile.style.float="left";
-tile.style.paddingBottom= "11.1%";
-tile.style.backgroundColor= color;
-body.appendChild(tile);
+  var tile= document.createElement('div');
+  var color= randomColor();
+  tile.style.width="11.1%";
+  tile.style.float="left";
+  tile.style.paddingBottom= "11.1%";
+  tile.style.backgroundColor= color;
+  body.appendChild(tile);
 }
+}
+
 
 function randomColor () {
 var letter="0123456789ABCDEF";
@@ -19,3 +23,6 @@ var color="#";
   }
   return color;
 }
+
+createCheckerboard();
+window.setInterval(createCheckerboard,200);
